d425b56a1598a5c02d7f7abd6fcad88521e2625e
Sarp Basaraner <sarpbasaraner@gmail.com>
2020-02-22 14:44:46 +0300

    parse init command

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
+use std::path::PathBuf;
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
 #[structopt(about = "The stupid content tracker")]
 enum Command {
-    Init
+    Init {
+        #[structopt(parse(from_os_str), default_value = ".")]
+        path: PathBuf
+    }
 }
 
 fn main() {
     let command = Command::from_args();
-    println!("{:#?}", command);
+    match command {
+        Command::Init { path } => init(path),
+    }
+}
+
+fn init(path: PathBuf) {
+    println!("{:#?}", path);
 }
